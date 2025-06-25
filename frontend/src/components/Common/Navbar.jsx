aa793fa29d7d535bb7b96e220af341f78af1eeb7
unknown <manoranjanjenamitsy17@gmail.com>
2025-06-26 00:14:17 +0530

    Done with the implementation of CartDrawer functionality in Navbar

--- frontend/src/components/Common/Navbar.jsx
+++ frontend/src/components/Common/Navbar.jsx
@@ -1,10 +1,17 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Link } from 'react-router-dom'
 import {HiOutlineUser, HiOutlineShoppingBag,HiBars3BottomRight} from 'react-icons/hi2'
 import SearchBar from './SearchBar';
 import CartDrawer from '../Layout/CartDrawer';
 
 const Navbar = () => {
+
+    const [drawerOpen, setDrawerOpen] = useState(false);
+
+    const toggleCartDrawer = () => {
+      setDrawerOpen(!drawerOpen);
+    };
+
   return (
     <>
       <nav className="container mx-auto flex items-center justify-between py-4 px-6">
@@ -48,7 +55,7 @@ const Navbar = () => {
           <Link to="/profile" className="hover:text-black">
             <HiOutlineUser className="h-6 w-6 text-gray-700" />
           </Link>
-          <button className="relative hover:text-black">
+          <button  onClick={toggleCartDrawer} className="relative hover:text-black">
             <HiOutlineShoppingBag className="h-6 w-6 text-gray-700" />
             <span className="absolute -top-1 bg-roxilier-red text-white text-xs rounded-full px-2 py-0.5">
               4
@@ -65,7 +72,7 @@ const Navbar = () => {
           </button>
         </div>
       </nav>
-      <CartDrawer/>
+      <CartDrawer drawerOpen={drawerOpen} toggleCartDrawer={toggleCartDrawer} />
 
     </>
   );
--- frontend/src/components/Layout/CartDrawer.jsx
+++ frontend/src/components/Layout/CartDrawer.jsx
@@ -1,12 +1,8 @@
 import React, { useState } from 'react'
 import { IoMdClose } from 'react-icons/io'
 
-const CartDrawer = () => {
-    const [drawerOpen,setDrawerOpen]=useState(true)
-
-    const toggleCartDrawer =()=>{
-        setDrawerOpen(!drawerOpen)
-    }
+const CartDrawer = ({drawerOpen,toggleCartDrawer}) => {
+  
   return (
     <div className={`fixed top-0 right-0 w-3/4 sm:w-1/2 md:w-1/4 h-full bg-white shadow-lg transform transition-transform duration-300 flex flex-col z-50 ${drawerOpen? "translate-x-0":"translate-x-full"}`}>
         {/* Close Button  */}
