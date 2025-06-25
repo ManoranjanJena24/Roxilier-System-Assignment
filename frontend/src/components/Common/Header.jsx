95665817314ae01d37c370693123161946259846
unknown <manoranjanjenamitsy17@gmail.com>
2025-06-25 23:24:31 +0530

    Done with the Header Search bar functionlity

--- frontend/src/components/Common/Header.jsx
+++ frontend/src/components/Common/Header.jsx
@@ -1,12 +1,16 @@
 import React from 'react'
 import Topbar from '../Layout/Topbar'
+import Navbar from './Navbar'
 
 const Header = () => {
   return (
-    <div>
+    <header className='border-b border-gray-200'>
+      
         {/* Topbar */}
         <Topbar/>
-    </div>
+
+        <Navbar/>
+    </header>
   )
 }
 
